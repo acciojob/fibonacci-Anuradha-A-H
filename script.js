8e2a316f65818d761562c47f61445b08d88c533a
Anuradha-A-H <119663653+Anuradha-A-H@users.noreply.github.com>
2024-07-29 21:03:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 function fibonacci(num) {
 // your code here
-	if(num <= 2) return 1;
-	return fibonacci(num-1)+fibonacci(num-2);
+	let arr = new Array(num+1).fill(0);
+	arr[1] = 1;
+	arr[2] = 1;
+	for(let i = 3;i<=num+1;i++)
+		{
+			arr[i] = arr[i-1]+arr[i-2];
+		}
+	return arr[num];
+	
 }
 
 module.exports = fibonacci;
